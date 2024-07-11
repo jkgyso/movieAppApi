26534fa5d6b48961ef51a648276bf2ea08b3fcc9
jkgyso <jkgyso@gmail.com>
2024-07-11 19:28:30 +0800

    revised user controller

--- controllers/user.js
+++ controllers/user.js
@@ -3,25 +3,28 @@ const bcrypt = require('bcryptjs');
 const auth = require('../auth');
 
 module.exports.registerUser = (req, res) => {
+    let newUser = new User({
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, 10),
+        mobileNo: req.body.mobileNo
+    });
 
-	let newUser = new User({
-		email: req.body.email,
-		password: bcrypt.hashSync(req.body.password, 10)
-	})
-
-	if (!req.body.email.includes("@")){
-	    return res.status(400).send({ error: 'Email invalid' });
-	} else if (req.body.password.length < 8) {
-	    return res.status(400).send({ error: 'Password must be atleast 8 characters' });
-	} else {
-
-		return newUser.save().then(user => res.status(201).send({ message: 'Registered Successfully'})).catch(saveErr => {
-
-			console.error('Error in saving the user: ', saveErr);
-
-			return res.status(500).send({ error: 'Error in Save' });
-		})
-	}
+    if (!req.body.email.includes("@")) {
+        return res.status(400).send({ error: 'Email invalid' });
+    }
+    else if (req.body.mobileNo.length !== 11) {
+        return res.status(400).send({ error: 'Mobile number invalid' });
+    }
+    else if (req.body.password.length < 8) {
+        return res.status(400).send({ error: 'Password must be at least 8 characters' });
+    } else {
+        return newUser.save()
+            .then(user => res.status(201).send({ message: 'Registered Successfully' }))
+            .catch(saveErr => {
+                console.error('Error in saving the user: ', saveErr);
+                return res.status(500).send({ error: 'Error in Save' });
+            });
+    }
 };
 
 module.exports.loginUser = (req, res) => {
@@ -58,7 +61,8 @@ module.exports.loginUser = (req, res) => {
 
 		return res.status(400).send({ error: 'Invalid in email'});
 	}
-};
+}
+
 
 module.exports.userDetails = (req, res) => {
 
