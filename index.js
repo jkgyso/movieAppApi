eaacd3d1872c49ac218c48cf4bff9688d160fb74
jkgyso <jkgyso@gmail.com>
2024-07-11 14:36:53 +0800

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
+
 
 const app = express();
 
@@ -7,6 +9,7 @@ const port = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 mongoose.connect('mongodb+srv://admin:admin1234@gysodb.nijqxz1.mongodb.net/Movie-Catalog-System?retryWrites=true&w=majority&appName=gysoDB');
 
